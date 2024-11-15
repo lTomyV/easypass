d7be8d3a8cd494ab977eab60d79673408fdde3e8
Tomy <50710890+lTomyV@users.noreply.github.com>
2024-11-15 17:37:51 -0300

    package(jest supertest)

--- dist/adapters/input/TicketController.js
+++ dist/adapters/input/TicketController.js
@@ -12,7 +12,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-// src/adapters/input/TicketController.ts
+// src/infrastructure/input/TicketController.ts
 const express_1 = __importDefault(require("express"));
 const BuyTicket_1 = require("../../core/app/BuyTicket");
 const MongoTicketRepository_1 = require("../output/MongoTicketRepository");
